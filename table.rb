75b2699c3e2aa4a5061ca8454a3f2d17ec1c3e5f
anasansari157 <anasansari157@gmail.com>
2020-08-05 18:38:09 +0530

    add create_table method

--- table.rb
+++ table.rb
@@ -5,5 +5,17 @@ class Table
     @table = create_table(table_data)
   end
 
+  def create_table(table_data) 
+    data_for_table = []
+    for item in table_data do 
+      data_for_table << [item.title, item.time, item.duration]
+    end
 
+    table = Terminal::Table.new :headings => ['Time', 'Talk', 'Duration'], :rows => data_for_table, :style => { :border_x => "-", :border_i => "-",:border_top => false, :border_bottom => false, :border_y => ""}
+
+    puts "\n"    
+    puts table
+    puts "\n"    
+
+  end
 end
