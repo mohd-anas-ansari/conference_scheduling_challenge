5cdfa74d25e7b022d0cdf4d2c4f9b9e12c623bc9
anasansari157 <anasansari157@gmail.com>
2020-08-05 18:29:10 +0530

    bundle static list of talks

--- talk_data.rb
+++ talk_data.rb
@@ -2,9 +2,34 @@ require_relative "talk"
 
   module Talk_data
     List = [
-      Talk.new("Talk 1", 30)
+      Talk.new("Riding Rails for 10 years lightning talk", 05),
+      Talk.new("Ansible : An alternative to chef lightning talk", 05),
+      Talk.new("Docker isn't just for deployment", 30),
+      Talk.new("Callbacks in Rails", 30),
+      Talk.new("Teaching github for poets", 60),    
+      Talk.new("Test Driving your Rails Infrastucture with Chef", 60),    
+      Talk.new("SVG charts and graphics with Ruby", 45),    
+      Talk.new("Interviewing like a unicorn: How Great Teams Hire", 30),    
+      Talk.new("How to talk to humans: a different approach to soft skills", 30),
+      Talk.new("Getting a handle on Legacy Code", 60),    
+      Talk.new("Heroku: A year in review", 30),    
+      Talk.new("Ruby on Rails on Minitest", 30),
+      Talk.new("JavaScript", 30),
+      Talk.new("Pryin open the black box", 60),
+      Talk.new("Migrating a huge production codebase from sinatra to Rails", 45),
+      Talk.new("How does bundler work", 30),
+      Talk.new("Sustainable Open Source", 45),
+      Talk.new("How to program with Accessiblity in Mind", 45),
+      Talk.new("Implementing a strong code review culture", 60),
+      Talk.new("Scaling Rails for Black Friday", 45)
     ]
   end 
 
 
-  p Talk_data::List
+
+
+
+
+
+
+  p Talk_data::List, "talk_data"
