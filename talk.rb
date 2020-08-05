edc3590936c44163cd207c5f4387a328d92eae3f
anasansari157 <anasansari157@gmail.com>
2020-08-05 18:25:16 +0530

    set respective fields of Talk

--- talk.rb
+++ talk.rb
@@ -1,10 +1,10 @@
 class Talk
   attr_accessor  :title, :duration, :time
 
-  def initialize talk, time=0
-    @title = talk.title
-    @duration = talk.duration
+  def initialize title, duration, time=0
+    @title = title
+    @duration = duration
     @time = time
   end
-  
+
 end
