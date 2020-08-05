5394fd137ce1b6fd73d66c7682b1133a4c582e5e
anasansari157 <anasansari157@gmail.com>
2020-08-05 19:18:35 +0530

    add all constants

---
+++ constants.rb
@@ -0,0 +1,18 @@
+module Constants 
+
+#time = starting time
+#duration = period
+
+  Time = {
+    :start_time             => 900,
+    :network_session_start_time  => 1700,
+    :lunch_start_time       => 1200,
+    :lunch_end_time         => 1300,
+    :lunch_duration           => 60,
+    :morning_duration         => 180,
+    :evening_duration         => 240,
+    :maximum_session_duration => 60,
+    :total_lightning_duration => 10
+  }
+
+end
